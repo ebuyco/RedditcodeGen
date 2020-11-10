4f9127768e6f68876dd3edec2d44647bb4ca2807
ErnieBuyco <buycoernie@gmail.com>
2020-11-10 22:34:38 +0800

    commit setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,92 +1,37 @@
-import "reflect-metadata";
-import "dotenv-safe/config";
-import { __prod__, COOKIE_NAME } from "./constants";
+import 'reflect-metadata';
+
+import { ApolloServer } from 'apollo-server-express';
+import { HelloResolver } from './resolvers/hello';
+import { MikroORM } from '@mikro-orm/core';
+import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
+import { __prod__ } from './constants';
+import {buildSchema} from 'type-graphql';
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
-import { HelloResolver } from "./resolvers/hello";
-import { PostResolver } from "./resolvers/post";
-import { UserResolver } from "./resolvers/user";
-import Redis from "ioredis";
-import session from "express-session";
-import connectRedis from "connect-redis";
-import cors from "cors";
-import { createConnection } from "typeorm";
-import { Post } from "./entities/Post";
-import { User } from "./entities/User";
-import path from "path";
-import { Updoot } from "./entities/Updoot";
-import { createUserLoader } from "./utils/createUserLoader";
-import { createUpdootLoader } from "./utils/createUpdootLoader";
+import microConfig from './mikro-orm.config';
 
 const main = async () => {
-  const conn = await createConnection({
-    type: "postgres",
-    url: process.env.DATABASE_URL,
-    logging: true,
-    // synchronize: true,
-    migrations: [path.join(__dirname, "./migrations/*")],
-    entities: [Post, User, Updoot],
-  });
-  // await conn.runMigrations();
-
-  // await Post.delete({});
-
+  const orm = await MikroORM.init(microConfig);    
+  await orm.getMigrator().up();
+  
+  
   const app = express();
-
-  const RedisStore = connectRedis(session);
-  const redis = new Redis(process.env.REDIS_URL);
-  app.set("trust proxy", 1);
-  app.use(
-    cors({
-      origin: process.env.CORS_ORIGIN,
-      credentials: true,
-    })
-  );
-  app.use(
-    session({
-      name: COOKIE_NAME,
-      store: new RedisStore({
-        client: redis,
-        disableTouch: true,
+  const apolloServer =  new ApolloServer({
+      schema: await buildSchema({
+          resolvers: [HelloResolver, PostResolver, UserResolver],
+          validate: false,
       }),
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
-        httpOnly: true,
-        sameSite: "lax", // csrf
-        secure: __prod__, // cookie only works in https
-        domain: __prod__ ? ".codeponder.com" : undefined,
-      },
-      saveUninitialized: false,
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-    })
-  );
-
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver, UserResolver],
-      validate: false,
-    }),
-    context: ({ req, res }) => ({
-      req,
-      res,
-      redis,
-      userLoader: createUserLoader(),
-      updootLoader: createUpdootLoader(),
-    }),
+      context: () =>({ em: orm.em })
   });
-
-  apolloServer.applyMiddleware({
-    app,
-    cors: false,
-  });
-
-  app.listen(parseInt(process.env.PORT), () => {
-    console.log("server started on localhost:4000");
-  });
+  
+apolloServer.applyMiddleware({ app });
+  
+  app.listen(4000, () => {
+    console.log('server start on localhost:4000');
+  })
+  
 };
 
-main().catch((err) => {
-  console.error(err);
+main().catch(err => {
+     console.error(err)   
 });
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,12 +1,5 @@
-import { Request, Response } from "express";
-import { Redis } from "ioredis";
-import { createUserLoader } from "./utils/createUserLoader";
-import { createUpdootLoader } from "./utils/createUpdootLoader";
+import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
 
 export type MyContext = {
-  req: Request & { session: Express.Session };
-  redis: Redis;
-  res: Response;
-  userLoader: ReturnType<typeof createUserLoader>;
-  updootLoader: ReturnType<typeof createUpdootLoader>;
-};
+    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+}
